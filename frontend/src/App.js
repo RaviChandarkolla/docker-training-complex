34ba7094325d4208a71b00cd6b3037f54ef2baa1
RaviChandarKolla <ravichandarkolla@gmail.com>
2022-06-30 15:34:11 +0530

    added github actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There, how  are you, i am fine
         </p>
         <a
           className="App-link"
