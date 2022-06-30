aaf8d8dc005945684167e685827bb1748c3c5475
RaviChandarKolla <ravichandarkolla@gmail.com>
2022-06-30 16:19:13 +0530

    checking github actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There, how  are you, i am fine
+          Checking Github actions
         </p>
         <a
           className="App-link"
